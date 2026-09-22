c0ff791a737b7d7d7dfa008d4a4422f0734eb638
mrfunnyboy72-dot <mrfunnyboy72@gmail.com>
2026-09-22 12:10:49 +0530

    Admin catalog live auto-sync to GitHub main

--- src/data/products.js
+++ src/data/products.js
@@ -1,5 +1,5 @@
 export const STORE_WHATSAPP_NUMBER = '919876543210';
-export const CATALOG_VERSION = 'v8_1790059236893';
+export const CATALOG_VERSION = 'v8_1790059249514';
 
 export const CATEGORIES = [
   {
